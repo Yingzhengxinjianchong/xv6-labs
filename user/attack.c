6abd7c99ab022d4c124c97619430c60fc24479a9
Yingzhengxinjianchong <2219704394@qq.com>
2025-07-09 00:51:13 +0800

    Finish the lab Attack xv6, which means lab2:system calls is finished

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,15 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  char *end = sbrk(PGSIZE * 32);
 
+  if (end == (char*)-1) {
+     fprintf(2, "attack: sbrk failed\n");
+     exit(1);
+  }
+
+  end = end + 16 * PGSIZE;
+  char *secret = end + 32;
+  write(2, secret, 8);
   exit(1);
 }
